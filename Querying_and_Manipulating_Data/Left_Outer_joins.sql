efbcfb319ef97e79beadc42fcefb20297042d85d
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-27 19:19:43 +0100

    Querying with Left Outer Joins

---
+++ Querying_and_Manipulating_Data/Left_Outer_joins.sql
@@ -0,0 +1,10 @@
+--Using the Inner Joins
+USE WideWorldImporters
+GO
+
+SELECT Sales.Customers_Archive.CustomerID, Sales.Orders.OrderID
+FROM [Sales].[Customers_Archive]
+LEFT OUTER JOIN [Sales].[Orders]
+ON Sales.Customers_Archive.CustomerID = Sales.Orders.CustomerID
+WHERE (Sales.Orders.OrderID IS NULL)
+ORDER BY Sales.Customers_Archive.CustomerID
