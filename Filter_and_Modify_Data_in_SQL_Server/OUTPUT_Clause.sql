9ef109a0f95cd093007bb51acee14a26bbf9ff2b
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-28 20:36:09 +0100

    Output Clause

---
+++ Filter_and_Modify_Data_in_SQL_Server/OUTPUT_Clause.sql
@@ -0,0 +1,13 @@
+USE TestDB
+GO
+
+DECLARE @RecordDeleted TABLE
+(ID Int,
+Name VARCHAR(12),
+Price MONEY)
+
+DELETE FROM MasterMenu
+OUTPUT DELETED * INTO @RecordDeleted
+WHERE MenuItemId = 4;
+
+SELECT * FROM @RecordDeleted
